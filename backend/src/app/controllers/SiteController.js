7f98fdec40285b5db86b746917baec1f3881b979
Nathan Cao <nhatcao0111@gmail.com>
2023-09-06 12:24:12 +0700

    update BE site APIs

--- backend/src/app/controllers/SiteController.js
+++ backend/src/app/controllers/SiteController.js
@@ -1,7 +1,46 @@
+const Recipe = require("../models/Recipe");
+const resClientData = require("../../utils/resClientData");
+
 class SiteController {
-  async index(req, res) {
-    try {
-    } catch (error) {}
+  // [GET] /api/v1/recipes/all
+  async all(req, res) {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    const data = await Recipe.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    if (!data) {
+      res.status(404);
+      throw new Error("Recipes not found");
+    }
+
+    const dataCount = await Recipe.countDocuments();
+
+    resClientData(res, 200, {
+      pagination: {
+        totalDocuments: dataCount,
+        totalPages: Math.ceil(dataCount / limit),
+        pageSize: limit,
+        currentPage: page,
+      },
+      data: data,
+    });
+  }
+
+  // [GET] /api/v1/recipes/:id
+  async single(req, res) {
+    const recipeId = req.params.id;
+    const data = await Recipe.findOne({ _id: recipeId });
+
+    if (!data) {
+      res.status(404);
+      throw new Error("Recipe not found");
+    }
+
+    resClientData(res, 200, data);
   }
 }
 
--- backend/src/routes/site.js
+++ backend/src/routes/site.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const siteController = require("../app/controllers/SiteController");
+const asyncHandler = require("express-async-handler");
 
-router.get("/", siteController.index);
+router.get("/all", asyncHandler(siteController.all));
+router.get("/:id", asyncHandler(siteController.single));
 
 module.exports = router;
